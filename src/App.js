18217f280c92bfce4c98bed795b6c5cb7629d1f7
renan <renan.rodriguesmoura2022@gmail.com>
2022-11-06 22:25:51 -0300

    primeiros codigos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [location, setLocation] = useState(false)
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      setLocation(true)
+    })
+  }, [])
+
+  if (location == false) {
+    return (
+      alert('a localização precisa estar ativa no browser para continuar')
+    );
+  } else {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Clima nas suas Coordenadas </h1>
+          <hr />
+          <ul>
+            <li>Temperatura Atual: </li>
+            <li>Temperatura maxima: </li>
+            <li>Temperatura minima: </li>
+            <li>Pressão:  </li>
+            <li>Umidade: </li>
+          </ul>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
