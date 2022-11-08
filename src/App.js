66107e8450c0ad1ce51de5cebf93a2a84c5c132d
renan <renan.rodriguesmoura2022@gmail.com>
2022-11-07 22:11:31 -0300

    acessando api

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [location, setLocation] = useState(false)
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  let getWeather = async (lat, long)=>{
+    let res = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
+      params:{
+        lat: lat,
+        lon: long,
+        appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
+        lang: 'pt',
+        units: 'metric'
+      }
+    });
+    setWeather(res.data);
+  }
+
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position.coords.latitude, position.coords.longitude);
+      getWeather(position.coords.latitude, position.coords.longitude);
       setLocation(true)
     })
-  }, [])
+  }, []);
 
   if (location == false) {
     return (
