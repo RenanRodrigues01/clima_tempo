1b20c02bb1c6c493107acaa920441008519b61a6
renan <renan.rodriguesmoura2022@gmail.com>
2022-11-08 22:44:00 -0300

    utilizando api

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   const [location, setLocation] = useState(false);
   const [weather, setWeather] = useState(false);
 
-  let getWeather = async (lat, long)=>{
+  let getWeather = async (lat, long) => {
     let res = await axios.get("https://api.openweathermap.org/data/2.5/weather", {
-      params:{
+      params: {
         lat: lat,
         lon: long,
         appid: process.env.REACT_APP_OPEN_WHEATHER_KEY,
@@ -30,20 +30,30 @@ function App() {
     return (
       alert('a localização precisa estar ativa no browser para continuar')
     );
-  } else {
+  } else if (weather == false) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>Clima nas suas Coordenadas </h1>
+      <div>
+        Carregando...
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className='container'>
+        <header className='cabecalho'>
+          <h1 className='title'>Clima em {weather['name']} ({weather['weather'][0]['description']}) </h1>
           <hr />
-          <ul>
-            <li>Temperatura Atual: </li>
-            <li>Temperatura maxima: </li>
-            <li>Temperatura minima: </li>
-            <li>Pressão:  </li>
-            <li>Umidade: </li>
-          </ul>
         </header>
+        <main>
+        <ul className='lista'>
+            <li className='lista-item'>Temperatura Atual: {weather['main']['temp']}°</li>
+            <li className='lista-item'>Temperatura maxima: {weather['main']['temp_max']}°</li>
+            <li className='lista-item'>Temperatura minima: {weather['main']['temp_min']}°</li>
+            <li className='lista-item'>Seansação térmica: {weather['main']['feels_like']}°</li>
+            <li className='lista-item'>Pressão: {weather['main']['pressure']}hpa</li>
+            <li className='lista-item'>Umidade: {weather['main']['humidity']}%</li>
+          </ul>
+        </main>
       </div>
     );
   }
